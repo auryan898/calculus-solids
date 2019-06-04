99924008c96d415b657502bb6ea6b91671670d16
Ryan Au <auryan898@gmail.com>
2019-06-04 10:50:55 -0400

    Fixed the logic

--- updateserver.py
+++ updateserver.py
@@ -1,13 +1,14 @@
-import pip
 try:
-    import pip._internal
-    install = None
     try:
+        import pip
         main = pip.main
     except:
+        import pip._internal
         main = pip._internal.main
 except:
     exit(1)
+if True or main("show Calculus-Solids-of-Known-Volume".split()) == 1:
+    main('install --upgrade https://github.com/auryan898/calculus-solids/archive/master.zip --user'.split())
 
-
-main('install https://github.com/auryan898/calculus-solids/archive/master.zip --user'.split())
+from calculus_solids import web
+web.start()
