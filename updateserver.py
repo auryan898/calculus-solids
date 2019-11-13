e47fb6c5eb983164dd6e08df33ac368b56401070
Ryan Au <auryan898@gmail.com>
2019-11-13 12:08:53 -0500

    add analytics to site

--- updateserver.py
+++ updateserver.py
@@ -7,8 +7,12 @@ try:
         main = pip._internal.main
 except:
     exit(1)
-if True or main("show Calculus-Solids-of-Known-Volume".split()) == 1:
-    main('install --upgrade https://github.com/auryan898/calculus-solids/archive/master.zip --user'.split())
+import sys
 
+version = '.'.join(map(str,sys.version_info[0:2]))
+if (version == '2.7' or version == '2.6') and True or main("show Calculus-Solids-of-Known-Volume".split()) == 1:
+    main('install --upgrade https://github.com/auryan898/calculus-solids/archive/master.zip --user'.split())
+else:
+    print('Use python 2.7 or 2.6 for best results. Will not install.')
 # from calculus_solids import web
 # web.start()
